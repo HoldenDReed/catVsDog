5b1613ebf86d0b9fc3ef28fd531f503c25f70f18
Holden D Reed <holdendreed@hotmail.com>
2022-10-03 20:40:48 -0500

    Added more Json

--- scripts/database.js
+++ scripts/database.js
@@ -1,7 +1,9 @@
 // fetch apis
+const API = "http://localhost:8080"
 const applicationState = {
     dogImage: [],
-    catImage: []
+    catImage: [],
+    history: []
 }
 
 export const fetchCat = async () => {
@@ -12,6 +14,32 @@ export const fetchCat = async () => {
 
 export const fetchDog = async () => {
     const dogImage = await fetch(`https://api.thedogapi.com/v1/images/search`)
-    const dogImageResponse = await catImage.json()
+    const dogImageResponse = await dogImage.json()
     applicationState.dogImage = dogImageResponse
 }
+
+export const fetchHistory = async () => {
+    const history = await fetch(`${API}/history`)
+    const historyResponse = await history.json()
+    applicationState.history = historyResponse
+}
+
+export const getHistory = () => {
+    const copyOfHistory = applicationState.history.map(history => ({ ...history }))
+    return copyOfHistory
+}
+
+export const addNewHistory = async (history) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(history)
+    }
+    const response = await fetch(`${API}/history`, fetchOptions)
+    const responseJson = await response.json()
+    document.dispatchEvent(new CustomEvent("stateChanged"))
+    return responseJson
+  }
+  
