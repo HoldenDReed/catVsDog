a91074c9e5fbfd16a7c1ded096a7f669f3125bdc
Chase Burnett <chaseburnett@me.com>
2022-10-04 21:36:59 -0500

    updated scoreboard

--- scripts/scoreboard.js
+++ scripts/scoreboard.js
@@ -1,1 +1,26 @@
+import { getFeed } from "./database.js";
+
+export const catScores = () => {
+    const feed = getFeed()
+     feed.filter((x) => { return x.catWin})
+}
+
+export const dogScores = () => {
+    const feed = getFeed()
+     feed.filter((x) => { return !x.catWin} )
+}
+
+const catScore = catScores().length
+const dogScore = dogScores().length
+
+document.getElementById('catScore').innerHTML = catScore
+document.getElementById('dogScore').innerHTML = dogScore
+
+
+
 // displays database of voted on pets
+
+//const catScore = []
+//const dogScore = []
+// catScore = feed.filter((x) => { return x.catWin})//
+// dogScore = feed.filter((x) => { return !x.catWin})//
