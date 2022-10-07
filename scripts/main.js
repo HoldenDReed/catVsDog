4a3535e57114e3c2947d5564660b0fb98d99c1ff
Mcshelton96 <mcshelton96@gmail.com>
2022-10-06 20:40:12 -0500

    added feed

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 // render everything
-import { fetchCat, fetchDog, fetchFeed } from "./database.js"
+import { fetchCat, fetchDog, fetchFeed, } from "./database.js"
+import { displayFeed } from "./feed.js"
+
+const feedContainer = document.getElementById('feed')
 
 const render = async () => {
     await fetchCat()
     await fetchDog()
     await fetchFeed()
+    feedContainer.innerHTML = displayFeed()
 }
 
 render()
