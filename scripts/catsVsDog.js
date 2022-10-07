93bec1dfaa628bdafef74144a2221d9ff9b6a966
JeremyK1830 <108768065+JeremyK1830@users.noreply.github.com>
2022-10-06 20:17:09 -0500

    Added random images to dom

--- scripts/catsVsDog.js
+++ scripts/catsVsDog.js
@@ -1,19 +1,29 @@
+import { getRandomCat, getRandomDog  } from "./database.js";
 // add random cat and dog imagies
+
+export const randomCat = () => {
+const randomCat = getRandomCat()
+   let catHTML = `<div class="catImage">
+    <p>${randomCat.id}</p>
+    <img src =${randomCat.url} alt="randomCat" class="randomCatImage">` 
+
+    return catHTML
+    }
+    
+   export const randomDog = () => {
+    const randomDog = getRandomDog()
+    let dogHTML =`<div class="dogImage">
+    <p>${randomDog.id}</p>
+    <img src= ${randomDog.url} alt="randomDog" class= "randomDogImage">`
+    
+    return dogHTML
+    }
+    document.getElementById("catCard").innerHTML = catHTML
+    document.getElementById("dogCard").innerHTML = dogHTML
 // add button to score with and refresh images
+
+
 // add current images to database
-// const displayPainting = async () => {
-//     for (const id of paintingsId) {
-//         const data = await fetchPaintings(id)
-//         renderPainting(data)
-//     }
-// }
-// const renderPainting = (data) => {
-// let html = 
-//     `<p>Tile: ${data.title}</p>
-//     <img src=${data.primaryImageSmall} alt=${data.title}>`
-
-// document.getElementById("app").innerHTML += html
 
 // }
 
-// displayPainting()
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
 // render everything
 import { fetchCat, fetchDog, fetchFeed } from "./database.js"
+import { randomCat, randomDog } from "./catsVsDog.js"
+
+const dogVsContainer = document.getElementById("dogCard") 
+const catVsContainer = document.getElementById("catCard") 
 
 const render = async () => {
     await fetchCat()
     await fetchDog()
     await fetchFeed()
+    dogVsContainer.innerHTML = randomDog
+    catVsContainer.innerHTML = randomCat
 }
 
 render()
