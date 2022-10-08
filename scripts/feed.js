902951ea3d4f205d551bcf627648b80f062d1d53
Holden D Reed <holdendreed@hotmail.com>
2022-10-08 09:47:38 -0500

    Added more csS

--- scripts/feed.js
+++ scripts/feed.js
@@ -6,13 +6,13 @@ export const displayFeed = () => {
     const feed = getFeed()
     let html = ""
     feed.forEach(entry => {
-    html += `
+        html += `
     <div class = "dogFeed">
         <div class ='dogPicBox'><img src = ${entry.dogUrl} alt ="Dog picture" class ="dogFeedPic"></div>
     </div>
     <div><iframe src="https://giphy.com/embed/IeiONINKriNrbSygju" width="120" height="250" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></div>
     <div class = "catFeed">
-        <div class ='catPicBox'><img src = ${entry.catUrl} alt ="cat picture" class ="catFeedPic"></div>
+        <div class ='catPicBox vsFeedGif'><img src = ${entry.catUrl} alt ="cat picture" class ="catFeedPic"></div>
     </div>
         `
     })
