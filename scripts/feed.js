f8e2fa76596d6c33dbee249f4724fed4e63ed8a1
Holden D Reed <holdendreed@hotmail.com>
2022-10-11 18:33:46 -0500

    Fixed flexbox

--- scripts/feed.js
+++ scripts/feed.js
@@ -7,12 +7,14 @@ export const displayFeed = () => {
     let html = ""
     feed.forEach(entry => {
         html += `
+        <div class="feedItem">
     <div class = "dogFeed">
         <div class ='dogPicBox'><img src = ${entry.dogUrl} alt ="Dog picture" class ="dogFeedPic ${entry.catWin ? '' : 'winner'}"></div>
     </div>
     <div><iframe src="https://giphy.com/embed/IeiONINKriNrbSygju" width="120" height="250" frameBorder="0" class="giphy-embed vsFeedGif" allowFullScreen></iframe></div>
     <div class = "catFeed">
         <div class ='catPicBox'><img src = ${entry.catUrl} alt ="cat picture" class ="catFeedPic ${entry.catWin ? 'winner' : ''}"></div>
+    </div>
     </div>`
     })
     return html
